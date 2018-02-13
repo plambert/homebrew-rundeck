01597fd07d387cdce96d0c4531258604d89975d0
Paul M. Lambert <plambert@plambert.net>
2018-02-13 14:24:38 -0800

    better debugging for rundeck server formula

--- rundeck.rb
+++ rundeck.rb
@@ -1,4 +1,4 @@
-# WARNING: THIS IS A DEVELOPMENT-ONLY VERSION OF THIS FORMULA
+# WARNING: THIS IS A DEVELOPMENT-ONLY VERSION OF THIS FORMULA, IT DOES NOT WORK!
 
 class Rundeck < Formula
   desc "Enable self-service access to existing scripts and tools."
@@ -7,15 +7,18 @@ class Rundeck < Formula
   sha256 "b6dde4ae74ad00a3c2fac3b05f894cf3e2d9e7e8e27a118e64d15813619f4458"
   depends_on :java => "1.8"
   def install
-    if Pathname.new("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz").file?
+    if File.file?("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz")
+      puts "Using cached build for formula development"
       system "tar", "-xzpf", "#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz"
     else
       system "make", "app"
-      if Pathname.new("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache").directory?
+      if File.directory?("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache")
+        puts "Creating cached build for future formula development"
         system "tar", "-czvf", "#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz"
       end
     end
   end
   test do
+    true
   end
 end
