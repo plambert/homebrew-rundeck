bed9c1b82907a24e904ea36ff63c4ca2fddce69c
Paul M. Lambert <plambert@plambert.net>
2018-02-13 16:23:12 -0800

    fast install testing is now possible

--- rundeck.rb
+++ rundeck.rb
@@ -3,20 +3,25 @@
 class Rundeck < Formula
   desc "Enable self-service access to existing scripts and tools."
   homepage "http://rundeck.org"
+  # for testing, do a 'brew install rundeck -i' and run 'make app && tar -czf ~/rundeck.build.tgz --exclude .brew* .'
+  # then uncomment this url and comment the real versions, as well as the 'system make app'
+  # you might want to remove any unneeded files between the 'make app' and 'tar' commands in order to
+  # reduce the size of the rundeck.build.tgz file considerably.
+  # url "file://#{ENV['HOME']}/rundeck.build.tgz"
+
+  # comment these two lines when testing
   url "https://github.com/rundeck/rundeck/archive/v2.10.6.tar.gz"
   sha256 "b6dde4ae74ad00a3c2fac3b05f894cf3e2d9e7e8e27a118e64d15813619f4458"
+  version "2.10.6"
   depends_on :java => "1.8"
   def install
-    if File.file?("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz")
-      puts "Using cached build for formula development"
-      system "tar", "-xzpf", "#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz"
-    else
-      system "make", "app"
-      if File.directory?("#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache")
-        puts "Creating cached build for future formula development"
-        system "tar", "-czvf", "#{ENV["HOME"]}/.cache/rundeck-server-homebrew-cache/rundeck.build.tgz"
-      end
-    end
+    # comment this line when testing
+    system "make", "app"
+    libexec.install "rundeck-launcher/launcher/build/libs/rundeck-launcher-#{version}.jar"
+    (bin/"rundeck-server").write <<~EOS
+      #!/usr/bin/env bash
+      JAVA_HOME="$(/usr/libexec/java_home -v 1.8)" exec java -jar "#{libexec}/rundeck-launcher-#{version}.jar" "$@"
+    EOS
   end
   test do
     true
