9b6e764e3eb13e05aedd204d0f4d7c4297f286af
Paul M. Lambert <plambert@plambert.net>
2018-04-15 11:02:22 -0700

    improvements still in progress...

--- rundeck-server.bash
+++ rundeck-server.bash
@@ -1,103 +1,102 @@
 #!/usr/bin/env bash
+      
+# Launch Rundeck server
 
-opts=( )
+# You can set options in $(brew --prefix)/etc/rundeck.conf
 
-# server.http.port
-# The HTTP port to use for the server
-RDECK_SERVER_HTTP_PORT="${RDECK_SERVER_HTTP_PORT:-4440}"
-[[ -n "$RDECK_SERVER_HTTP_PORT" ]] && opts=( "${opts[@]}" "-Dserver.http.port=${RDECK_SERVER_HTTP_PORT}" )
+BREW_PREFIX="$(brew --prefix)" || exit $?
+jarfile="#{prefix}/libexec/rundeck-launcher-#{version}.jar"
 
-# server.https.port
-# The HTTPS port to use or the server
-RDECK_SERVER_HTTPS_PORT="${RDECK_SERVER_HTTPS_PORT:-4443}"
-[[ -n "$RDECK_SERVER_HTTPS_PORT" ]] && opts=( "${opts[@]}" "-Dserver.https.port=${RDECK_SERVER_HTTPS_PORT}" )
+# Get options from ${BREW_PREFIX}/etc/rundeck.conf
 
-# server.http.host
-# Address/hostname to listen on
-RDECK_SERVER_HTTP_HOST="${RDECK_SERVER_HTTP_HOST:-127.0.0.1}"
-[[ -n "$RDECK_SERVER_HTTP_HOST" ]] && opts=( "${opts[@]}" "-Dserver.http.host=${RDECK_SERVER_HTTP_HOST}" )
+declare -a opts
+declare -a vars
 
-# server.hostname
-# Hostname to use for the server
-RDECK_SERVER_HOSTNAME="${RDECK_SERVER_HOSTNAME:-Pesto.attlocal.net}"
-[[ -n "$RDECK_SERVER_HOSTNAME" ]] && opts=( "${opts[@]}" "-Dserver.hostname=${RDECK_SERVER_HOSTNAME}" )
+export JAVA_HOME
 
-# server.web.context
-# Web context path to use, such as "/rundeck"
-RDECK_SERVER_WEB_CONTEXT="${RDECK_SERVER_WEB_CONTEXT:-/}"
-[[ -n "$RDECK_SERVER_WEB_CONTEXT" ]] && opts=( "${opts[@]}" "-Dserver.web.context=${RDECK_SERVER_WEB_CONTEXT}" )
+RDECK_RDECK_BASE="${BREW_PREFIX}/var/rundeck"
 
-# rdeck.base
-# Rundeck Basedir to use
-RDECK_RDECK_BASE="${RDECK_RDECK_BASE:-$(brew --prefix)/var/rundeck}"
-[[ -n "$RDECK_RDECK_BASE" ]] && opts=( "${opts[@]}" "-Drdeck.base=${RDECK_RDECK_BASE}" )
+# Default settings to pass to Rundeck
+# shellcheck disable=SC2034
+{
+  RDECK_DEFAULT_USER_NAME="${USER:-admin}"
+  RDECK_SERVER_HOSTNAME="localhost"
+  RDECK_SERVER_HTTP_HOST=127.0.0.1
+  RDECK_SERVER_HTTP_PORT=4440
+}
 
-# server.datastore.path
-# Path to server datastore dir
-RDECK_SERVER_DATASTORE_PATH="${RDECK_SERVER_DATASTORE_PATH:-$(brew --prefix)/var/db/rundeck}"
-[[ -n "$RDECK_SERVER_DATASTORE_PATH" ]] && opts=( "${opts[@]}" "-Dserver.datastore.path=${RDECK_SERVER_DATASTORE_PATH}" )
+if [[ -f "${BREW_PREFIX}/etc/rundeck.conf" ]]; then
+  # shellcheck disable=SC1090
+  source "${BREW_PREFIX}/etc/rundeck.conf" || exit $?
+fi
 
-# default.user.name
-# Username for default user account to create
-RDECK_DEFAULT_USER_NAME="${RDECK_DEFAULT_USER_NAME:-admin}"
-[[ -n "$RDECK_DEFAULT_USER_NAME" ]] && opts=( "${opts[@]}" "-Ddefault.user.name=${RDECK_DEFAULT_USER_NAME}" )
+# for a given pair of option name and variable name, add the option
+# to the list of options, only if the named variable is not undefined or empty
+add_opt_for_var() {
+  local opt var
+  while [[ $# -gt 1 ]]; do
+    opt="$1"; var="$2"; shift 2
+    if [[ -n "${!var}" ]]; then
+      opts=( "${opts[@]}" "-D${opt}=${!var}" )
+    fi
+    vars=( "${vars[@]}" "$var" )
+  done
+}
 
-# default.user.password
-# Password for default user account to create
-RDECK_DEFAULT_USER_PASSWORD="${RDECK_DEFAULT_USER_PASSWORD:-admin}"
-[[ -n "$RDECK_DEFAULT_USER_PASSWORD" ]] && opts=( "${opts[@]}" "-Ddefault.user.password=${RDECK_DEFAULT_USER_PASSWORD}" )
+# shellcheck disable=SC2153
+debug() {
+  declare -p -v "${vars[@]}"
+}
 
-# rundeck.jaaslogin
-# if true, enable JAAS login, otherwise use the realm.properties file for login information
-RDECK_RUNDECK_JAASLOGIN="${RDECK_RUNDECK_JAASLOGIN:-false}"
-[[ -n "$RDECK_RUNDECK_JAASLOGIN" ]] && opts=( "${opts[@]}" "-Drundeck.jaaslogin=${RDECK_RUNDECK_JAASLOGIN}" )
+debug_exec() {
+  if [[ -n "$RDECK_SERVER_SCRIPT_DEBUG" ]]; then
+    printf 1>&2 '%q ' "$@"; printf 1>&2 '\n'
+  fi
+  if [[ "$RDECK_SERVER_SCRIPT_DEBUG" != 'dryrun' ]]; then
+    exec "$@"
+  else
+    printf 1>&2 '\e[31m%s: not actually running server due to dryrun mode\e[0m\n' "$(basename -- "$0")"
+  fi
+}
 
-# loginmodule.name
-# Custom JAAS loginmodule name to use
-RDECK_LOGINMODULE_NAME="${RDECK_LOGINMODULE_NAME:-}"
-[[ -n "$RDECK_LOGINMODULE_NAME" ]] && opts=( "${opts[@]}" "-Dloginmodule.name=${RDECK_LOGINMODULE_NAME}" )
+add_opt_for_var \
+  server.http.port       RDECK_SERVER_HTTP_PORT       \
+  server.https.port      RDECK_SERVER_HTTPS_PORT      \
+  server.http.host       RDECK_SERVER_HTTP_HOST       \
+  server.hostname        RDECK_SERVER_HOSTNAME        \
+  server.web.context     RDECK_SERVER_WEB_CONTEXT     \
+  rdeck.base             RDECK_RDECK_BASE             \
+  server.datastore.path  RDECK_SERVER_DATASTORE_PATH  \
+  default.user.name      RDECK_DEFAULT_USER_NAME      \
+  default.user.password  RDECK_DEFAULT_USER_PASSWORD  \
+  rundeck.jaaslogin      RDECK_RUNDECK_JAASLOGIN      \
+  loginmodule.name       RDECK_LOGINMODULE_NAME       \
+  loginmodule.conf.name  RDECK_LOGINMODULE_CONF_NAME  \
+  rundeck.config.name    RDECK_RUNDECK_CONFIG_NAME    \
+  rundeck.ssl.config     RDECK_RUNDECK_SSL_CONFIG     \
+  rundeck.jetty.connector.forwarded \
+    RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED \
+  rundeck.jetty.connector.ssl.excludedProtocols \
+    RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS \
+  rundeck.jetty.connector.ssl.includedProtocols \
+    RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS \
+  rundeck.jetty.connector.ssl.excludedCipherSuites \
+    RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES \
+  rundeck.jetty.connector.ssl.includedCipherSuites \
+    RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES
 
-# loginmodule.conf.name
-# Name of a custom JAAS config file, located in the server's config dir.
-RDECK_LOGINMODULE_CONF_NAME="${RDECK_LOGINMODULE_CONF_NAME:-}"
-[[ -n "$RDECK_LOGINMODULE_CONF_NAME" ]] && opts=( "${opts[@]}" "-Dloginmodule.conf.name=${RDECK_LOGINMODULE_CONF_NAME}" )
+if [[ -n "$RDECK_SERVER_SCRIPT_DEBUG" ]]; then
+  debug
+fi
 
-# rundeck.config.name
-# Name of a custom rundeck config file, located in the server's config dir.
-RDECK_RUNDECK_CONFIG_NAME="${RDECK_RUNDECK_CONFIG_NAME:-}"
-[[ -n "$RDECK_RUNDECK_CONFIG_NAME" ]] && opts=( "${opts[@]}" "-Drundeck.config.name=${RDECK_RUNDECK_CONFIG_NAME}" )
+cd "$RDECK_RDECK_BASE" || exit $?
 
-# rundeck.ssl.config
-# Path to the SSL config properties file to enable SSL. If not set, SSL is not enabled.
-RDECK_RUNDECK_SSL_CONFIG="${RDECK_RUNDECK_SSL_CONFIG:-}"
-[[ -n "$RDECK_RUNDECK_SSL_CONFIG" ]] && opts=( "${opts[@]}" "-Drundeck.ssl.config=${RDECK_RUNDECK_SSL_CONFIG}" )
+JAVA_HOME="$(/usr/libexec/java_home -v 1.8)" || exit $?
 
-# rundeck.jetty.connector.forwarded
-# Set to true to enable support for "X-forwarded-*" headers which may be sent by a front-end proxy to the rundeck server (see "Using an SSL Terminated Proxy")
-RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED="${RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED:-false}"
-[[ -n "$RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED" ]] && opts=( "${opts[@]}" "-Drundeck.jetty.connector.forwarded=${RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED}" )
+if [[ -z "$JAVA_HOME" ]] || [[ ! -d "$JAVA_HOME" ]]; then
+  printf 1>&2 '%s %s: java home directory not found\n' "$0" "$JAVA_HOME"
+  exit 3
+fi
 
-# rundeck.jetty.connector.ssl.excludedProtocols
-# Comma-separated list of SSL protocols to disable (see Disabling SSL Protocols)
-RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS="${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS:-SSLv3}"
-[[ -n "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS" ]] && opts=( "${opts[@]}" "-Drundeck.jetty.connector.ssl.excludedProtocols=${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS}" )
+debug_exec java "${opts[@]}" -jar "$jarfile" "-b" "${BREW_PREFIX}/var/rundeck" "$@"
 
-# rundeck.jetty.connector.ssl.includedProtocols
-# Comma-separated list of SSL protocols to include (see Disabling SSL Protocols)
-RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS="${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS:-}"
-[[ -n "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS" ]] && opts=( "${opts[@]}" "-Drundeck.jetty.connector.ssl.includedProtocols=${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS}" )
-
-# rundeck.jetty.connector.ssl.excludedCipherSuites
-# Comma-separated list of Cipher suites to disable (see Disabling SSL Protocols)
-RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES="${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES:-}"
-[[ -n "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES" ]] && opts=( "${opts[@]}" "-Drundeck.jetty.connector.ssl.excludedCipherSuites=${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES}" )
-
-# rundeck.jetty.connector.ssl.includedCipherSuites
-# Comma-separated list of Cipher suites to enable (see Disabling SSL Protocols)
-RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES="${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES:-}"
-[[ -n "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES" ]] && opts=( "${opts[@]}" "-Drundeck.jetty.connector.ssl.includedCipherSuites=${RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES}" )
-
-JAVA_HOME="$(/usr/libexec/java_home -v 1.8)" exec java \
-  "${opts[@]}" \
-  -jar "#{libexec}/rundeck-launcher-#{version}.jar" \
-"$@"
--- rundeck.rb
+++ rundeck.rb
@@ -1,59 +1,66 @@
 # WARNING: THIS IS A DEVELOPMENT-ONLY VERSION OF THIS FORMULA, IT DOES NOT WORK!
+require "English"
 
 class Rundeck < Formula
-  @@testing=true
-  @@prebuilt=File.file?("#{ENV['HOME']}/rundeck.build.tgz")
+  @testing=false
+  @name='rundeck2'
+  @prebuilt=File.file?("#{ENV["HOME"]}/#{@name}.build.tgz")
   desc "Enable self-service access to existing scripts and tools."
   homepage "http://rundeck.org"
-  local_url="file://#{ENV['HOME']}/rundeck.build.tgz"
+  local_url="file://#{ENV["HOME"]}/rundeck.build.tgz"
   remote_url="https://github.com/rundeck/rundeck/archive/v2.10.6.tar.gz"
 
-  if @@testing and @@prebuilt
+  if @testing && @prebuilt
     url local_url
     puts "==> Using #{local_url} in testing mode"
     version "2.10.6"
   else
     url remote_url
     sha256 "b6dde4ae74ad00a3c2fac3b05f894cf3e2d9e7e8e27a118e64d15813619f4458"
-    puts "==> Using #{remote_url} in testing mode" if @@testing
+    puts "==> Using #{remote_url} in testing mode" if @testing
   end
   depends_on :java => "1.8"
   def install
     # comment this line when testing
-    if @@testing and @@prebuilt
+    if @testing && @prebuilt
       puts "==> using prebuilt tgz in testing mode"
     else
       puts "==> making app"
       system "make", "app"
     end
-    if @@testing and ! @@prebuilt
+    if @testing && !@prebuilt
       puts "==> creating prebuilt tgz in testing mode"
-      system 'tar', '-czf', '~/rundeck.build.tgz', '--exclude', '.brew*', '.'
+      system "tar", "-czf", "~/rundeck.build.tgz", "--exclude", ".brew*", "."
     end
-    libexec.install "rundeck-launcher/launcher/build/libs/rundeck-launcher-#{version}.jar"
-    (var/"rundeck").mkdir #/ # fix sublime text ruby parser
+    libexec.install "rundeck-launcher/launcher/build/libs/#{@name}-launcher-#{version}.jar"
+    (var/@name).mkdir unless (var/@name).exist?
+    (var/"log").install_symlink (var/"#{@name}/server/logs") => @name
     (
-      bin/"rundeck-server" #/ # fix sublime text ruby parser
+      bin/"#{@name}-server"
     ).write <<~EOS
       #!/usr/bin/env bash
-      
+            
       # Launch Rundeck server
 
-      # You can set options in $(brew --prefix)/etc/rundeck.conf
-      
+      # You can set options in $(brew --prefix)/etc/#{@name}.conf
+
       BREW_PREFIX="$(brew --prefix)" || exit $?
-      jarfile="#{prefix}/libexec/rundeck-launcher-#{version}.jar"
-      
-      # Get options from ${BREW_PREFIX}/etc/rundeck.conf
-      
+      jarfile="#{prefix}/libexec/#{@name}-launcher-#{version}.jar"
+
+      # Get options from ${BREW_PREFIX}/etc/#{@name}.conf
+
       declare -a opts
 
-      RDECK_RDECK_BASE="${BREW_PREFIX}/var/rundeck"
+      export JAVA_HOME
+
+      RDECK_RDECK_BASE="${BREW_PREFIX}/var/#{@name}"
       RDECK_SERVER_HOSTNAME="localhost"
       RDECK_SERVER_HTTP_HOST=127.0.0.1
-      
-      if [[ -f "${BREW_PREFIX}/etc/rundeck.conf" ]]; then
-        source "${BREW_PREFIX}/etc/rundeck.conf" || exit $?
+      RDECK_SERVER_HTTP_PORT=4440
+
+      if [[ -f "${BREW_PREFIX}/etc/#{@name}.conf" ]]; then
+        # shellcheck disable=SC1090
+        source "${BREW_PREFIX}/etc/#{@name}.conf" || exit $?
       fi
             
       add_opt_for_var() {
@@ -63,53 +70,84 @@ class Rundeck < Formula
           [[ -n "${!var}" ]] && opts=( "${opts[@]}" "-D${opt}=${!var}" )
         done
       }
-      
-      add_opt_for_var \
-        server.http.port       RDECK_SERVER_HTTP_PORT       \
-        server.https.port      RDECK_SERVER_HTTPS_PORT      \
-        server.http.host       RDECK_SERVER_HTTP_HOST       \
-        server.hostname        RDECK_SERVER_HOSTNAME        \
-        server.web.context     RDECK_SERVER_WEB_CONTEXT     \
-        rdeck.base             RDECK_RDECK_BASE             \
-        server.datastore.path  RDECK_SERVER_DATASTORE_PATH  \
-        default.user.name      RDECK_DEFAULT_USER_NAME      \
-        default.user.password  RDECK_DEFAULT_USER_PASSWORD  \
-        rundeck.jaaslogin      RDECK_RUNDECK_JAASLOGIN      \
-        loginmodule.name       RDECK_LOGINMODULE_NAME       \
-        loginmodule.conf.name  RDECK_LOGINMODULE_CONF_NAME  \
-        rundeck.config.name    RDECK_RUNDECK_CONFIG_NAME    \
-        rundeck.ssl.config     RDECK_RUNDECK_SSL_CONFIG     \
-        rundeck.jetty.connector.forwarded \
-          RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED \
-        rundeck.jetty.connector.ssl.excludedProtocols \
-          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS \
-        rundeck.jetty.connector.ssl.includedProtocols \
-          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS \
-        rundeck.jetty.connector.ssl.excludedCipherSuites \
-          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES \
-        rundeck.jetty.connector.ssl.includedCipherSuites \
+
+      # shellcheck disable=SC2153
+      debug() {
+        printf '%s=%q\\n' \\
+          RDECK_DEFAULT_USER_NAME "$RDECK_DEFAULT_USER_NAME" \\
+          RDECK_DEFAULT_USER_PASSWORD "$RDECK_DEFAULT_USER_PASSWORD" \\
+          RDECK_LOGINMODULE_CONF_NAME "$RDECK_LOGINMODULE_CONF_NAME" \\
+          RDECK_LOGINMODULE_NAME "$RDECK_LOGINMODULE_NAME" \\
+          RDECK_RDECK_BASE "$RDECK_RDECK_BASE" \\
+          RDECK_RUNDECK_CONFIG_NAME "$RDECK_RUNDECK_CONFIG_NAME" \\
+          RDECK_RUNDECK_JAASLOGIN "$RDECK_RUNDECK_JAASLOGIN" \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED "$RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED" \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES" \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS" \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES" \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS "$RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS" \\
+          RDECK_RUNDECK_SSL_CONFIG "$RDECK_RUNDECK_SSL_CONFIG" \\
+          RDECK_SERVER_DATASTORE_PATH "$RDECK_SERVER_DATASTORE_PATH" \\
+          RDECK_SERVER_HOSTNAME "$RDECK_SERVER_HOSTNAME" \\
+          RDECK_SERVER_HTTPS_PORT "$RDECK_SERVER_HTTPS_PORT" \\
+          RDECK_SERVER_HTTP_HOST "$RDECK_SERVER_HTTP_HOST" \\
+          RDECK_SERVER_HTTP_PORT "$RDECK_SERVER_HTTP_PORT" \\
+          RDECK_SERVER_WEB_CONTEXT "$RDECK_SERVER_WEB_CONTEXT"
+
+        printf 'Java Options:'
+        printf ' %q' "${opts[@]}"
+        printf '\\n'
+      }
+
+      add_opt_for_var \\
+        server.http.port       RDECK_SERVER_HTTP_PORT       \\
+        server.https.port      RDECK_SERVER_HTTPS_PORT      \\
+        server.http.host       RDECK_SERVER_HTTP_HOST       \\
+        server.hostname        RDECK_SERVER_HOSTNAME        \\
+        server.web.context     RDECK_SERVER_WEB_CONTEXT     \\
+        rdeck.base             RDECK_RDECK_BASE             \\
+        server.datastore.path  RDECK_SERVER_DATASTORE_PATH  \\
+        default.user.name      RDECK_DEFAULT_USER_NAME      \\
+        default.user.password  RDECK_DEFAULT_USER_PASSWORD  \\
+        rundeck.jaaslogin      RDECK_RUNDECK_JAASLOGIN      \\
+        loginmodule.name       RDECK_LOGINMODULE_NAME       \\
+        loginmodule.conf.name  RDECK_LOGINMODULE_CONF_NAME  \\
+        rundeck.config.name    RDECK_RUNDECK_CONFIG_NAME    \\
+        rundeck.ssl.config     RDECK_RUNDECK_SSL_CONFIG     \\
+        rundeck.jetty.connector.forwarded \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_FORWARDED \\
+        rundeck.jetty.connector.ssl.excludedProtocols \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDPROTOCOLS \\
+        rundeck.jetty.connector.ssl.includedProtocols \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDPROTOCOLS \\
+        rundeck.jetty.connector.ssl.excludedCipherSuites \\
+          RDECK_RUNDECK_JETTY_CONNECTOR_SSL_EXCLUDEDCIPHERSUITES \\
+        rundeck.jetty.connector.ssl.includedCipherSuites \\
           RDECK_RUNDECK_JETTY_CONNECTOR_SSL_INCLUDEDCIPHERSUITES
-      
+
+      if [[ -n "$RDECK_SERVER_SCRIPT_DEBUG" ]]; then
+        debug
+      fi
+
       cd "$RDECK_RDECK_BASE" || exit $?
-      
-      export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)" || exit $?
-      
-      if [[ ! -n "$JAVA_HOME" ]] || [[ ! -d "$JAVA_HOME" ]]; then
+
+      JAVA_HOME="$(/usr/libexec/java_home -v 1.8)" || exit $?
+
+      if [[ -z "$JAVA_HOME" ]] || [[ ! -d "$JAVA_HOME" ]]; then
         printf 1>&2 '%s %s: java home directory not found\n' "$0" "$JAVA_HOME"
         exit 3
       fi
-      
-      exec java "${opts[@]}" -jar "$jarfile" "-b" "${BREW_PREFIX}/var/rundeck" "$@"
-      
+
+      exec java "${opts[@]}" -jar "$jarfile" "-b" "${BREW_PREFIX}/var/#{@name}" "$@"
+
     EOS
   end
   test do
-    rc=0
-    if File.file?('/usr/local/bin/shellcheck')
-      system '/usr/local/bin/shellcheck', (bin/'rundeck-server') #/ # fix ruby syntax in sublime text
-      raise $? if $? > 0
+    if File.file?("/usr/local/bin/shellcheck")
+      system "/usr/local/bin/shellcheck", (bin/"rundeck-server")
+      raise $CHILD_STATUS if $CHILD_STATUS > 0
     end
-    system 'bash', '-n', (bin/'rundeck-server') #/ # fix ruby syntax in sublime text
-    raise $? if $? > 0
+    system "bash", "-n", (bin/"rundeck-server")
+    raise $CHILD_STATUS if $CHILD_STATUS > 0
   end
 end
