dca4fb9f54e45fbbd916e518389b28254982ce1c
Paul M. Lambert <plambert@plambert.net>
2018-04-25 15:26:19 -0400

    Add bottle for 1.0.25

--- rundeck-cli.rb
+++ rundeck-cli.rb
@@ -1,8 +1,13 @@
 class RundeckCli < Formula
   desc "CLI tool for Rundeck, the Service Reliability Engineering platform"
-  homepage "http://www.rundeck.com/"
+  homepage "https://www.rundeck.com/"
   url "https://github.com/rundeck/rundeck-cli/archive/v1.0.25.tar.gz"
   sha256 "74f5f7bbea0aa2d92a50e63e399251683c3aa4d662f0ccce7de287e2f94e502b"
+  bottle do
+    root_url "https://plambert.net/rundeck"
+    cellar :any_skip_relocation
+    sha256 "6eb39d6354714737a01376059f51e9e2d4413968070e3999c37e3f5a5f13ea04" => :high_sierra
+  end
   head "https://github.com/rundeck/rundeck-cli.git", :branch => "master"
   depends_on :java => "1.8"
   def install
