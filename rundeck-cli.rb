f5ecca54fbe11395f80e74bf159c2e0173110865
Paul M. Lambert <plambert@rundeck.com>
2018-09-25 22:00:49 -0400

    update to 1.1.0

--- rundeck-cli.rb
+++ rundeck-cli.rb
@@ -1,12 +1,12 @@
 class RundeckCli < Formula
   desc "CLI tool for Rundeck, the Service Reliability Engineering platform"
   homepage "https://www.rundeck.com/"
-  url "https://github.com/rundeck/rundeck-cli/archive/v1.0.25.tar.gz"
-  sha256 "74f5f7bbea0aa2d92a50e63e399251683c3aa4d662f0ccce7de287e2f94e502b"
+  url "https://github.com/rundeck/rundeck-cli/archive/v1.1.0.tar.gz"
+  sha256 "bd86a71e2934b68357acd0a688439a99cb9eb00aeb13c143b595d1f9f6b068e3"
   bottle do
     root_url "https://plambert.net/rundeck"
     cellar :any_skip_relocation
-    sha256 "6eb39d6354714737a01376059f51e9e2d4413968070e3999c37e3f5a5f13ea04" => :high_sierra
+    sha256 "7c5199a17738bfa107dd205d439d49e1dc0ba47ace10b9ac2ea0e735f0cf810e" => :high_sierra
   end
   head "https://github.com/rundeck/rundeck-cli.git", :branch => "master"
   depends_on :java => "1.8"
